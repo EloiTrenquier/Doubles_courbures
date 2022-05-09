be45e6325b0316a2249ddd9a24319912018fd7b1
EloiTrenquier <eloi.trenquier@gmail.com>
2022-05-09 20:21:38 +0200

    Adjacency matrix

--- PavingGraph.py
+++ PavingGraph.py
@@ -17,8 +17,13 @@ eps = 1e-4
 
 
 class Vertex:
-    """A vertex is a set of two sub_vertices, it corresponds to an edge of our Sphere Paving"""
+    """A vertex is a set of two sub_vertices, it corresponds to an edge of our Sphere Paving
 
+        If this (real life) edge is on the outside (Used by only one quad),
+    then the subvertices should be in the clockwise order for the quad it is a part of.
+        If it is on the inside (Used by two quads) then subvertices are in the clockwise order for the oldest one
+    and the anticlockwise order for the new one,
+    but it should be okay because we can't 'build' anything in this edge anymore"""
     def __init__(self, sub_vertex1, sub_vertex2, length, id=None):
         """ The id will be used to get the position of the Vertex in the Vertex list"""
         self._id = id
@@ -44,10 +49,12 @@ class SubVertex:
 
     def __init__(self, parent_id, in_edge_id=None, out_edge_id=None, id=None):
         """ A SubVertex may be part of multiple vertices, to recognize a SubVertex we use its id"""
-        self._parent_id = parent_id
-        self._in_edge_id = in_edge_id
-        self._out_edge_id = out_edge_id
-        self._id = id
+        self._parent_id = parent_id #id of the edge it is in
+        self._in_edge_id = in_edge_id #id of the edge that goes from another subvertex to this one
+        self._out_edge_id = out_edge_id #id of the edge that goes from this vertex to another
+        self._id = id #Name of this subvertex, unique
+        #A subvertex can represent the same vertex in "real life" ('A' for instance),
+        #A subvetex representing A is connected to another subvertex by a red edge iff this one also represents A
 
     @property
     def id(self):
@@ -86,12 +93,14 @@ class SubVertex:
 class Edge:
     """An Edge here corresponds to the connection of two edges on the sphere, it's weight is the angle between them """
 
-    def __init__(self, vertex1, vertex2, subvertex1, subvertex2, angle, id=None):
+    def __init__(self, vertex1, vertex2, subvertex1, subvertex2, angle, color, id=None):
         """ The id will be used to get the position of the Vertex in the Vertex list"""
         self._id = id
         self._vertices = [vertex1, vertex2]
-        self._subvertices = [subvertex1, subvertex2]
+        self._subvertices = [subvertex1, subvertex2] #The first one is the one this edge originates from,
+                                                    # the second is the destination
         self._weight = angle
+        self._color = color
 
     @property
     def id(self):
@@ -108,6 +117,13 @@ class Edge:
     @property
     def weight(self):
         return self._weight
+
+    @property
+    def color(self):
+        return self._color
+
+    def set_color(self, color):
+        self._color = color
 
     def set_id(self, id):
         out_subv, in_subv = self._subvertices[0], self._subvertices[1]
@@ -179,6 +195,7 @@ class PavingGraph:
             self._next_edge_id += 1
 
     def total_adjacency_mat(self):
+        """ Generates the adjacency matrix independently of whether the edges are red or blue"""
         N = len(self._subvertices)
         adj_mat = np.zeros((N, N))
         for edge in self._edges:
@@ -195,7 +212,37 @@ class PavingGraph:
 
         If it is possible to add a quad here, does so and returns 0
         else returns -1"""
-        check_list = quad.sides[0] == vertex.length #Potentiellement ajouter une tolérance d'erreur (?)
+        check_list = quad.sides[side] == vertex.length #Potentiellement ajouter une tolérance d'erreur (?)
+        free_pass = False
+        # We need to check for each edge of our quad whether it is free
+        # or it coincides with another real life edge (a graph vertex)
+        # 1st, we test for the 1st edge (in the clockwise order == edge[0] if we rotate the quad once anticlockwise)
+        p1 = vertex.points[0]
+        tot_angle_p1 = quad.beta #The first angle clockwise
+        #Either p1.in_edge_id or p1.out_edge_id should be none
+        assert p1.in_edge_id is None or p1.out_edge_id is None
+        while p1.in_edge_id is not None:
+            la_edge = self._edges[p1.in_edge_id]
+            tot_angle_p1 += la_edge.weight
+            p1 = la_edge.subvertices[0]
+        while p1.out_edge_id is not None:
+            la_edge = self._edges[p1.out_edge_id]
+            tot_angle_p1 += la_edge.weight
+            p1 = la_edge.subvertices[1]
+        if tot_angle_p1 > 2*pi: #+tolerance if we want some tolerance
+            return -1
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
